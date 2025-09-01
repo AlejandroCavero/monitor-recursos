acf853983a77a2d59e20982dec91c0966dd203f4
AlejandroCavero <alejandrojimenezcavero@gmail.com>
2025-09-01 20:06:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Monitor-recursos hecho por Alejandro Jiménez
+La app es un dashboard que muestra el uso de CPU, RAM y memoria de la máquina en la que se tenga corriendo la API, estos datos se muestran en un dashboard en tiempo real con la posibilidad de mandar alertas por telegram si se dan ciertos momentos en el sistema.
+
+Está hecho con HTML, CSS, JavaScript y Python con FastAPI
---
+++ app-linux/api.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import psutil
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/monitor")
+
+async def monitor():
+    #LA CPU ESTA EN % y LA MEMORIA EN GB
+    USO_CPU = psutil.cpu_percent(interval=1)
+    USO_DISCO = psutil.disk_usage('/').used / (1024 ** 3)
+    LIBRE_DISCO = psutil.disk_usage('/').free / (1024 ** 3)
+    USO_RAM = psutil.virtual_memory().used / (1024 ** 3)
+    LIBRE_RAM = psutil.virtual_memory().free / (1024 ** 3)
+
+    return {
+        "uso_cpu" : round(float(USO_CPU), 2),
+        "uso_disco" : round(float(USO_DISCO), 2),
+        "libre_disco" : round(float(LIBRE_DISCO), 2),
+        "uso_ram" : round(float(USO_RAM), 2),
+        "libre_ram" : round(float(LIBRE_RAM), 2)
+        }
